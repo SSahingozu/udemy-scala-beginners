7e74a7900f2c005ac7418a34882e43a094a66b3a
SevkanSahingozu <sevkansahingozu@hotmail.com>
2023-03-24 09:11:14 +0000

    added sequence object

---
+++ src/main/scala/lectures/part3fp/Sequences.scala
@@ -0,0 +1,11 @@
+package lectures.part3fp
+
+object Sequences extends App {
+
+  //Seq
+  val aSequence = Seq(1,2,3,4)
+  println(aSequence)
+  println(aSequence.reverse)
+  println(aSequence(2))
+  println(aSequence ++ Seq(5,6,7))
+}
